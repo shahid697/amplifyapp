cd52f1ba620e4b23fbd1fe872f0c3e2e3651be8a
shahid697 <shahidkhan412356@gmail.com>
2025-08-28 11:54:42 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React from 'react'; 
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-    <h1>Hello from V2</h1> 
-        <p>
-          <code>Hello from V2</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import logo from './logo.svg'; 
+import './App.css'; 
+ 
+function App() { 
+  return ( 
+    <div className="App"> 
+      <header className="App-header"> 
+        <img src={logo} className="App-logo" alt="logo" /> 
+        <h1>Hello from V2</h1> 
+      </header> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
